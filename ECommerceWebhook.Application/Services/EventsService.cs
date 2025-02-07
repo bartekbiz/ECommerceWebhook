b1d96921f2600cd636c452d04cdd1706b86c89fc
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2025-02-07 19:54:20 +0100

    Implement Event handling method

--- ECommerceWebhook.Application/Services/EventsService.cs
+++ ECommerceWebhook.Application/Services/EventsService.cs
@@ -8,21 +8,29 @@ public class EventsService : IEventsService
 {
     private readonly IEventsRepository _eventsRepository;
     private readonly IMapper _mapper;
+    private readonly IWebhooksService _webhooksService;
+    private readonly IWebhookNotifier _webhookNotifier;
 
-    public EventsService(IEventsRepository eventsRepository, IMapper mapper)
+    public EventsService(IEventsRepository eventsRepository, IMapper mapper, 
+        IWebhooksService webhooksService, IWebhookNotifier webhookNotifier)
     {
         _eventsRepository = eventsRepository;
         _mapper = mapper;
+        _webhooksService = webhooksService;
+        _webhookNotifier = webhookNotifier;
     }
     
     public async Task HandleAsync(string eventName, string orderNumber)
     {
-        if (!await _eventsRepository.ExistsAsync(eventName))
+        var eventEntity = await _eventsRepository.GetByNameAsync(eventName);
+        if (eventEntity == null)
         {
             throw new ArgumentException($"Event \"{eventName}\" does not exist.", nameof(eventName));
         }
-        
-        // TODO Invoke notifier
+
+        var webhooksToNotify = await _webhooksService.GetByEventIdAsync(eventEntity.Id);
+        var urlsToNotify = webhooksToNotify.Select(s => s.Url);
+        await _webhookNotifier.NotifyAsync(eventName, orderNumber, urlsToNotify);
     }
 
     public async Task<IEnumerable<EventResponseDto>> GetAllAsync()
