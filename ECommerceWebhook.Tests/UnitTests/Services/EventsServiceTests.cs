844478f65050b5d1eb6da49abf4c778f41ca2a7e
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2025-02-08 13:11:56 +0100

    Implement tests for EventsService

--- ECommerceWebhook.Tests/UnitTests/Services/EventsServiceTests.cs
+++ ECommerceWebhook.Tests/UnitTests/Services/EventsServiceTests.cs
@@ -1,4 +1,5 @@
 using ECommerceWebhook.Application.Services;
+using ECommerceWebhook.Domain.Entities;
 using ECommerceWebhook.Domain.Ports;
 using Xunit.Abstractions;
 
@@ -23,17 +24,93 @@ public class EventsServiceTests : ServiceTestsBase
     }
     
     [Fact]
-    public void HandleAsync_ShouldInvokeNotifierWithProperUrls()
+    public async Task HandleAsync_ShouldInvokeNotifierWithProperUrls()
     {
+        // Arrange
+        const int eventId = 1;
+        const string eventName = "event1";
+        const string orderNumber = "number";
+        var urls = new List<string>()
+        {
+            "http://some.com/1", 
+            "http://some.com/1"
+        };
+        
+        _mockEventsRepository
+            .Setup(x => x.GetByNameAsync(eventName))
+            .ReturnsAsync(
+                new Event
+                {
+                    Id = eventId,
+                    Name = eventName 
+                });
+
+        _mockWebhooksRepository
+            .Setup(x => x.GetByEventIdAsync(eventId))
+            .ReturnsAsync([
+                new Webhook()
+                {
+                    Id = 1,
+                    EventId = eventId,
+                    Url = urls[0]
+                },
+                new Webhook()
+                {
+                    Id = 2,
+                    EventId = eventId,
+                    Url = urls[1]
+                }
+            ]);
+        
+        var notifierInvoked = false;
+        _mockWebhooksNotifier
+            .Setup(x => x.NotifyAsync(eventName, orderNumber, urls))
+            .Callback(() => notifierInvoked = true);
+        
+        // Act
+        await _eventsService.HandleAsync(eventName, "number");
+
+        // Assert
+        Assert.True(notifierInvoked);
     }
     
     [Fact]
-    public void HandleAsync_ShouldFail()
+    public async Task HandleAsync_ShouldFail()
     {
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() => 
+            _eventsService.HandleAsync("name", "number"));
     }
     
     [Fact]
-    public void GetAllAsync_ShouldGetAllAndMapEventsToEventResponseDtos()
+    public async Task GetAllAsync_ShouldGetAllAndMapEventsToEventResponseDtos()
     {
+        // Arrange
+        const string event1Name = "event1";
+        
+        _mockEventsRepository
+            .Setup(x => x.GetAllAsync())
+            .ReturnsAsync([
+                new Event
+                {
+                    Id = 1,
+                    Name = event1Name 
+                },
+                new Event
+                {
+                    Id = 2,
+                    Name = "event2" 
+                }
+            ]);
+
+        // Act
+        var result = await _eventsService.GetAllAsync();
+        result = result.ToList();
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+        Assert.Equal(event1Name, result.First().Name);
+        Assert.Equal(2, result.Last().Id);
     }
 }
